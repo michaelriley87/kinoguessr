991c215b3f8c60fd63cbabae1218821e94a3e604
Michael Riley <michaelriley87@gmail.com>
2023-12-30 23:06:10 +1100

    commit1

--- kino-guessr-frontend/src/App.js
+++ kino-guessr-frontend/src/App.js
@@ -1,28 +1,32 @@
 import './App.css';
-import React from 'react';
-import { Box, Grid, Paper } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Container, Grid, Paper, TextField, Button } from '@mui/material';
 import headerImage from './images/header.png';
 import blankImage from './images/blank.png';
+import blankImage2 from './images/blank2.png';
 
 function App() {
   return (
     <div style={{ backgroundColor: '#8eb7d4', height: '100vh' }}>
-      <Box display="flex" justifyContent="center" alignItems="center" padding={2} >
+      <Box display="flex" justifyContent="center" alignItems="center" padding={1} >
         <img src={headerImage} alt="Header" style={{ maxWidth: '100%', width: '40vw', height: 'auto' }} />
       </Box>
-      
+      <Container maxWidth="md">
       <Grid container spacing={1} justifyContent="center">
-        {Array.from({ length: 5 }).map((_, index) => (
-          <Grid item xs={2} key={index}>
+        {Array.from({ length: 6 }).map((_, index) => (
+          <Grid item xs={4} key={index}>
             <Paper elevation={3} style={{ padding: '5px' }}>
-              <img src={blankImage} 
-              alt={`Actor ${index + 1}`} 
-              style={{ width: '100%', height: 'auto' }} 
+              <img 
+                src={index === 5 ? blankImage2 : blankImage}
+                alt={`Actor ${index + 1}`} 
+                style={{ width: '100%', height: 'auto' }} 
               />
             </Paper>
           </Grid>
         ))}
       </Grid>
+      </Container>
+
     </div>
   );
 }
